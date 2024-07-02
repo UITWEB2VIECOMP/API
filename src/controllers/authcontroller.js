837deaf8246e306c371c7790b77e8ed4bac109cd
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-07-02 21:04:40 +0700

    commit

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -2,15 +2,14 @@ const db = require('../../database')
 const bcrypt = require('bcrypt')
 const mysql = require('mysql2')
 
-exports.register= async(req, res)=>{
-    console.log('in reg');
+exports.register = async(req, res)=>{
     const {firstname, lastname,DOB, email, password, passwordConfirm} = req.body
     try{
         const [check] = await db.query('SELECT email FROM users where email = ?', [email]);
         if(check.length > 0){
-            return res.status(400).json({message: 'email in use'})
+            return res.status(400).json({status: "error", message: 'email in use'})
         }else if(password !== passwordConfirm){
-            return res.status(400).json({message: 'password do not match'})
+            return res.status(400).json({status: "error", message: 'password do not match'})
         }
         let hashedPassword = await bcrypt.hash(password, 8);
         const [roles] = await db.query('SELECT role_id FROM roles WHERE role_name = ?', ['student']);
@@ -19,7 +18,28 @@ exports.register= async(req, res)=>{
         const userID = insertUser.insertId;
         const [insertParticipant] = await db.query('INSERT INTO Participants SET ?', { user_id: userID, first_name: firstname, last_name: lastname, dob: DOB });
         console.log(insertParticipant);
-        return res.status(200).json({ message: 'User registered' });
+        return res.status(200).json({ status: "success",message: 'User registered' });
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({status: "error", message: 'Internal server error' });
+    }
+}
+
+exports.login = async(req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        return res.status(400).json({status: "error", message: "Plese enter your email and password"})
+    }
+    try{
+        const [emailCheck] = await db.query('SELECT email, password_hash ,user_id , role_id FROM users WHERE email = ?', [email]);
+        console.log(emailCheck);
+        if(emailCheck.length === 0 || !await bcrypt.compare(password, emailCheck[0].password_hash)){
+            return res.status(400).json({status: "error", message: "email or password is incorrect"}) 
+        }
+        else{
+            let [role] = await db.query('SELECT role_name FROM roles WHERE role_id = ?', [emailCheck[0].role_id]);
+            return res.status(200).send({status:'success', data:{user_id: emailCheck[0].user_id, role: role[0].role_name}})
+        }
     }catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error' });
