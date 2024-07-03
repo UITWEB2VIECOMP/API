e950f31b4e9c72195bc36734150a0c2452fe61bb
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-07-03 22:15:34 +0700

    [ADD]

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -19,8 +19,10 @@ exports.register = async(req, res)=>{
         const [insertUser] = await db.query('INSERT INTO users SET ?', { email: email, password_hash: hashedPassword, role_id: roleID });
         const userID = insertUser.insertId;
         await db.query('INSERT INTO Participants SET ?', { user_id: userID, first_name: firstname, last_name: lastname, dob: DOB });
+
         const token_value = crypto.randomBytes(32).toString("hex")
-        await db.query('INSERT INTO tokens SET ?',{user_id: userID, token: token_value})
+        const expireDate = new Date(Date.now() + 3600000);
+        await db.query('INSERT INTO tokens SET ?',{user_id: userID, token: token_value,token_type: "emailverify", expires_at: expireDate })
 
         const url = `${process.env.VERIFY_BASE_URL}auth/${userID}/verify/${token_value}`
         await sendEmail(email, "Verify email", url)
@@ -35,15 +37,16 @@ exports.register = async(req, res)=>{
 
 exports.verify = async(req,res)=>{
     try{
+        await db.query('DELETE FROM tokens WHERE expires_at < ?', [new Date()]);
         const {id, token } = req.params
         const [userCheck] = await db.query('SELECT user_id FROM users WHERE user_id = ?', [id]);
         if(userCheck.length === 0){return res.status(400).json({status:"error",message:"Invalid link"})}
-        const [tokenCheck] = await db.query('SELECT user_id, token FROM tokens WHERE user_id = ? AND token=?', [id, token])
+        const [tokenCheck] = await db.query('SELECT user_id, token FROM tokens WHERE user_id = ? AND token=? AND token_type = ?', [id, token, "emailverify"])
 
-        if(tokenCheck.length === 0){return res.status(400).json({status:"error",message:"Invalid link"})}
+        if(tokenCheck.length === 0){return res.status(400).json({status:"error",message:"Invalid link or link is expired"})}
 
         await db.query('UPDATE users SET verified = TRUE WHERE user_id = ?',[id])
-        await db.query('DELETE FROM tokens WHERE user_id = ?',[id])
+        await db.query('DELETE FROM tokens WHERE user_id = ? AND token_type = ?',[id, 'emailverify'])
         
         return res.status(200).json({status:"success", message:"email verified successfully"})
     }catch (error) {
@@ -55,6 +58,7 @@ exports.verify = async(req,res)=>{
 exports.resend = async(req, res)=>{
     const { email } = req.body;
     try{
+        await db.query('DELETE FROM tokens WHERE expires_at < ?', [new Date()]);
         const [emailCheck] = await db.query('SELECT user_id, verified FROM users WHERE email = ?', [email]);
 
         if (emailCheck.length === 0) {
@@ -63,10 +67,11 @@ exports.resend = async(req, res)=>{
         if (emailCheck[0].verified) {
             return res.status(400).json({ status: "error", message: "Email is already verified" });
         }
-        const [tokenCheck] = await db.query('SELECT token FROM tokens WHERE user_id = ?', [emailCheck[0].user_id]);
+        const [tokenCheck] = await db.query('SELECT token FROM tokens WHERE user_id = ? AND token_type = ?', [emailCheck[0].user_id, "emailverify"]);
         if (tokenCheck.length === 0) {
             const token_value = crypto.randomBytes(32).toString("hex")
-            await db.query('INSERT INTO tokens SET ?',{user_id: emailCheck[0].user_id, token: token_value})
+            const expireDate = new Date(Date.now() + 3600000);
+            await db.query('INSERT INTO tokens SET ? ',{user_id: emailCheck[0].user_id, token: token_value,token_type: "emailverify", expires_at: expireDate})
             const url = `${process.env.VERIFY_BASE_URL}auth/${emailCheck[0].user_id}/verify/${token_value}`
             await sendEmail(emailCheck[0].email, "Verify email", url)
         }
@@ -80,29 +85,38 @@ exports.resend = async(req, res)=>{
     }
 }
 
+exports.forgetpassword = async(req, res)=>{
+    const {email} = req.body
+    try{
+        const [emailCheck] = await db.query('SELECT email, password_hash ,user_id , role_id, verified FROM users WHERE email = ?', [email]);
+        if(emailCheck.length === 0){
+            return res.status(400).json({status: "error", message: "email is not exist"}) 
+        }
+        const token = crypto.randomBytes(32).toString('hex');
+
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({status: "error", message: 'Internal server error' });
+    }
+}
+
 exports.login = async(req, res)=>{
     const {email, password} = req.body
     if(!email || !password){
         return res.status(400).json({status: "error", message: "Plese enter your email and password"})
     }
     try{
+        await db.query('DELETE FROM tokens WHERE expires_at < ?', [new Date()]);
         const [emailCheck] = await db.query('SELECT email, password_hash ,user_id , role_id, verified FROM users WHERE email = ?', [email]);
         if(emailCheck.length === 0 || !await bcrypt.compare(password, emailCheck[0].password_hash)){
             return res.status(400).json({status: "error", message: "email or password is incorrect"}) 
         }
         console.log(emailCheck[0].verified);
         if(!emailCheck[0].verified){
-            const [tokenCheck] = await db.query('SELECT token FROM tokens WHERE user_id = ?', [emailCheck[0].user_id])
-            if(tokenCheck.length === 0){   
-                const token_value = crypto.randomBytes(32).toString("hex")
-                await db.query('INSERT INTO tokens SET ?',{user_id: userID, token: token_value})
-                const url = `${process.env.VERIFY_BASE_URL}auth/${userID}/verify/${token_value}`
-                await sendEmail(emailCheck[0].email, "Verify email", url)
-            }
             return res.status(400).json({status:"error",message:"An email is sent to your account please check"})
         }
         let [role] = await db.query('SELECT role_name FROM roles WHERE role_id = ?', [emailCheck[0].role_id]);
-        return res.status(200).send({status:'success', data:{user_id: emailCheck[0].user_id, role: role[0].role_name}})
+        return res.status(200).json({status:'success', data:{user_id: emailCheck[0].user_id, role: role[0].role_name}})
         
     }catch (error) {
         console.error(error);
