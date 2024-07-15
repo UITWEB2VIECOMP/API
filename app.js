944bd498fdca63ce4d80d7c70715f2f4fc59735f
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-07-15 20:31:24 +0700

    [UPDATE]

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const mysql = require('mysql2')
 const dotenv = require('dotenv')
 const app = express();
+const cors = require('cors');
+
 dotenv.config({path:'./.env'})
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', require('./src/routes/auth'))
 app.use('/api/users', require('./src/routes/users'))
