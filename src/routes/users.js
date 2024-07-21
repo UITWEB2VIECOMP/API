75f9168bb602e0cfbdbaad3b8d040efb99612baa
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-07-21 12:51:54 +0700

    [UPDATE]

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const {changePassword, uploadAvatar, getUser, changeAddress, changeName, changeDOB, changeContactInfo} = require('../controllers/userscontroller')
+const {changePassword, uploadAvatar, getUser, changeAddress, changeName, changeDOB, changeContactInfo, changeDescription} = require('../controllers/userscontroller')
 const router = express.Router()
 const {checkAuth} = require('../../middleware/checkAuth')
 const {uploadImg, multerErrorHandling}=require('../../middleware/multer')
 router.use(checkAuth)
 
-router.post('/upload-avatar', uploadImg, uploadAvatar);
+router.post('/change-avatar', uploadImg, uploadAvatar);
 router.post('/change-password', changePassword)
 router.get('/get-user', getUser)
 router.post('/change-name', changeName)
 router.post('/change-dob', changeDOB)
 router.post('/change-address', changeAddress)
 router.post('/change-contact', changeContactInfo)
+router.post('/change-description', changeDescription)
 
 
 router.use(multerErrorHandling)
