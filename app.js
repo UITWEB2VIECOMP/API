6bf8daf57aa49a62143462ebb44ec161c0c2e116
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2024-07-26 15:40:38 +0700

    UPDATE

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const cors = require('cors');
 dotenv.config({path:'./.env'})
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-app.use(cors())
-
+app.use(cors({
+    origin: true
+}))
 app.use('/api/auth', require('./src/routes/auth'))
 app.use('/api/users', require('./src/routes/users'))
 app.use('/api/homepage', require('./src/routes/homepage'))
